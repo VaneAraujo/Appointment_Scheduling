e0cabb33b72b104ef2fc747729e7df6bed7ec59f
VaneAraujo <113569291+VaneAraujo@users.noreply.github.com>
2023-03-30 11:23:54 -0500

    Patients WEB

--- Appointment.WEB/Program.cs
+++ Appointment.WEB/Program.cs
@@ -1,4 +1,5 @@
 using Appointment.WEB;
+using Appointment.WEB.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
