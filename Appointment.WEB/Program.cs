ec9b9aeaa93dd9927af21ad117cc1159db60d581
VaneAraujo <113569291+VaneAraujo@users.noreply.github.com>
2023-03-30 13:48:47 -0500

    Errores

--- Appointment.WEB/Program.cs
+++ Appointment.WEB/Program.cs
@@ -1,5 +1,6 @@
 using Appointment.WEB;
 using Appointment.WEB.Repositories;
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
--- Appointment.WEB/Repositories/HttpResponseWrapper.cs
+++ Appointment.WEB/Repositories/HttpResponseWrapper.cs
@@ -18,7 +18,7 @@ namespace Appointment.WEB.Repositories
 
         public HttpResponseMessage HttpResponseMessage { get; set; }
 
-        public async Task<string?> GetErrorMessage()
+        public async Task<string?> GetErrorMessageAsync()
         {
             if (!Error)
             {
